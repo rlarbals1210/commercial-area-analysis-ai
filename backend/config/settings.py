8a5bc2dca3ea2d365dd74ae1748135c4ddfb5e6e
rlarbals1210 <rlarbals1230@naver.com>
2026-03-26 15:55:39 +0900

    secret-key 보관 수정

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o420l$yo5cf3^zyw)b7*5hitrvekj=ta)shl2d!eu=zy&ixg&u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-o420l$yo5cf3^zyw)b7*5hitrvekj=ta)shl2d!eu=zy&ixg&u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')]
 
 
 # Application definition
@@ -58,12 +58,14 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
 
-
-# CORS_ALLOWED_ORIGINS =[
-#     "http://localhost:5173",
-# ]
+VERCEL_URL = os.environ.get('VERCEL_URL', '')
+if VERCEL_URL:
+    CORS_ALLOWED_ORIGINS.append(f"https://{VERCEL_URL}")
 
 
 
